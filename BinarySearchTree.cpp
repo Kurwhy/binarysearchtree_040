5192648317b61f111636512c36bddd56d4e43594
Wahyu <firmansyahw503@gmail.com>
2023-06-29 14:13:34 +0700

    memasukkan element baru ke struktur data m

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -28,5 +28,21 @@ public:
 		ROOT = NULL; // Initializing ROOT to null
 	}
 
-	
+	void insert(string element) // Insert a node in the binary search tree
+	{
+		Node* newNode = new Node(element, NULL, NULL); // Allocate memory for the new node
+		newNode->info = element; // Assign value to the data field of the new node
+		newNode->leftchild = NULL; // Make the left child of the new node point to NULL
+		newNode->rightchild = NULL; // Make the right child of the new node point to NULL
+
+		Node* parent = NULL;
+		Node* currentNode = NULL;
+		search(element, parent, currentNode); // Locate the node which will be the parent of the node to be inserted
+
+		if (parent == NULL) // if the perent is NULL (tree is empty
+		{
+			ROOT = newNode; // Mark the new node as ROOT
+			return; // EXIT
+		}
+	}
 };
