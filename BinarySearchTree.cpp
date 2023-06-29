2f928e47363a278fee1730b98d6c69a76abc0a75
Wahyu <firmansyahw503@gmail.com>
2023-06-29 18:18:43 +0700

    15

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -99,4 +99,14 @@ public:
 			preorder(ptr->rightchild);
 		}
 	}
+
+	void postorder(Node* ptr)
+	{
+		// Performs the postorder traversal of the tree
+		if (ROOT == NULL)
+		{
+			cout << "Tree is empty" << endl;
+			return;
+		}
+	}
 };
